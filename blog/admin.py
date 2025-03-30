bbd7fc1b930d9a42931c90800e55bd85bec16efa
ahadulhaquenaim <ahadulhaquenaim1@gmail.com>
2025-03-30 15:13:44 +0600

    feat:add models for post and author and tang

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Post, Author, Tag
+
 
 # Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    list_filter = ('tags', 'author', 'date')
+    list_display = ('title', 'author', 'date')
+    prepopulated_fields = {'slug': ('title',)}  # Automatically fill the slug field based on the title
+
+
+admin.site.register(Post, PostAdmin)  # Register the Post model with the custom admin class
+admin.site.register(Author)
+admin.site.register(Tag)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
+
 
 # Create your models here.
+
+class Tag(models.Model):
+    caption = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.caption}"
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email_address = models.EmailField(unique=True)
+
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def __str__(self):
+        return self.full_name()
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
+    image_name = models.CharField(max_length=100)
+    date = models.DateField(auto_now=True)
+    excerpt = models.CharField(max_length=200)
+    content = models.TextField(validators=[MinLengthValidator(10)])
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL, related_name='posts', null=True)
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return f"{self.title} {self.date}"
