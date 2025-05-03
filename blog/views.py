f2c58130d5517590ad15d9814a8922bc60ec9da2
ahadulhaquenaim <ahadulhaquenaim1@gmail.com>
2025-05-03 22:18:44 +0600

    feat:add comment form

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 from .models import Post
 from django.views.generic import ListView, DetailView
+from .forms import CommentForm
 
 
 # Create your views here.
@@ -37,4 +38,5 @@ class SinglePostView(DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['post_tags'] = self.object.tags.all()
+        context['comment_form'] = CommentForm()
         return context
